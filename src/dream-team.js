32803fc0e2cc21fe43f35da0998f6b06b1e554dc
wecansur <vlkob95@gmail.com>
2022-09-08 17:38:45 +0800

    feat: delete white spaces and make names upper cased

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  const upperCaseMembersWithoutWhiteSpaces = [];
+  members.forEach((item) => {
+    let name = '';
+    for (let i = 0; i < item.length; i++) {
+      if(item[i] !== ' ') {
+        name += item[i].toUpperCase();
+      }
+    }
+    upperCaseMembersWithoutWhiteSpaces.push(name);
+  })
+  
 }
 
 module.exports = {
