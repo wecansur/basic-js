29486c725d50cbed8ba416931a9ab070f59b8fcd
wecansur <vlkob95@gmail.com>
2022-09-08 22:13:45 +0800

    feat: make one circle of sum-digits

--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,18 +12,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(n) {
+ function getSumOfDigits(n) {
   const str = String(n);
-  let sum = str[0];
-  for (let i = 1; i < str.length; i++) {
-    sum+= str[i];
-  }
-  numSum = Number(sum)
-  if (sum.length === 1) {
-    return numSum;
-  } else {
-    getSumOfDigits(numSum)
-  }
+    let sum = Number(str[0]);
+      for (let i = 1; i < str.length; i++) {
+        sum+= Number(str[i]);
+      }
+      numSum = Number(sum);
+      return numSum;
 }
 
 module.exports = {
