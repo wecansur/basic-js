8dbd18ab9db78da56458f8a9c5135399de2acb18
wecansur <vlkob95@gmail.com>
2022-09-08 18:14:38 +0800

    feat: make what-season for correct values

--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+/*
+spring = 2, 3, 4
+summer = 5, 6, 7
+autumn = 8, 9, 10
+winter = 11, 0, 1
+*/
+function getSeason(date) {
+  const month = date.getMonth();
+  if (month === 11 || month <= 1) {
+    return 'winter';
+  } else if (month > 1 && month <= 4) {
+    return 'spring';
+  } else if (month > 4 && month <= 7) {
+    return 'summer';
+  } else if (month > 7 && month <= 10) {
+    return 'autumn';
+  }
 }
 
 module.exports = {
