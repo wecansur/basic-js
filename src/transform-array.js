df54c92232cca8242f24ede380bc7e0f662490a2
wecansur <vlkob95@gmail.com>
2022-09-09 12:29:40 +0800

    feat: make transform-array for valid values

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  const result = [];
+  let i = 0;
+  while (i < arr.length) {
+    if (typeof(arr[i]) === 'number') {
+      result.push(arr[i]);
+      i++;
+    } else if (arr[i] === '--discard-next') {
+      i = i + 2;
+    } else if (arr[i] === '--discard-prev') {
+      if (result[result.length - 1] === arr[i - 1]) {
+        result.pop()
+      }
+      i++;
+    } else if (arr[i] === '--double-next') {
+      result.push(arr[i + 1]);
+      i++;
+    } else if (arr[i] === '--double-prev') {
+      result.push(arr[i - 1]);
+      i++;
+    }
+  }
+  return result;
 }
 
 module.exports = {
