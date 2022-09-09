f6d5dc891f972b9796f9898b5e09380ca2aa8767
wecansur <vlkob95@gmail.com>
2022-09-09 16:43:57 +0800

    feat: make repeatTimes in ext-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = str;
+  for (let i = 2; i <= options.repeatTimes; i++) {
+    result += `+${str}`;
+  }
+  return result;
 }
 
 module.exports = {
