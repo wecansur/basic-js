453d27d64c5978824a686ee50adcb788acdb43b5
wecansur <vlkob95@gmail.com>
2022-09-09 13:35:48 +0800

    feat: chaining works in simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+   this.chain.push(`( ${value} )`);
+   return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+   this.chain = this.chain.filter((_, index) => index !== position - 1);
+   return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+   this.chain.reverse();
+   return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.join('~~')
   }
 };
 
